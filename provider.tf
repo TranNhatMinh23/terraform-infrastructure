0c9268980d47b5821260e1e04a5752d540d83b57
Minh Tran <trannhatminh23082001@gmail.com>
2023-10-03 13:49:26 +0700

    .

--- provider.tf
+++ provider.tf
@@ -15,11 +15,11 @@ terraform {
   }
   
   backend "s3" {
-    bucket         = "backend-eos-terraform-3"   
+    bucket         = "backend-eos-terraform-5"   
     key            = "terraform.tfstate"            
     region         = "ap-southeast-1"                   
     encrypt        = true     
-    dynamodb_table = "tabledb"                 
+    dynamodb_table = "dynamodbtb"                 
   }
 
 }
